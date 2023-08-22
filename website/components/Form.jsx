473b221e6ca7437788decfdbfd4126d6116e0310
Guillaume Ducuing <86024187+lecodeurnormand@users.noreply.github.com>
2023-08-22 20:17:43 +0200

    Update - Components - Form - add laptop & phone

--- website/components/Form.jsx
+++ website/components/Form.jsx
@@ -5,9 +5,12 @@ const Form = () => {
   const [kilometers, setKilometers] = useState('')
   const [meatConsumed, setMeatConsumed] = useState('')
   const [flightHours, setFlightHours] = useState('')
+  const [laptop, setLaptop] = useState('')
+  const [phone, setPhone] = useState('')
   const [totalCarbonFootprint, setTotalCarbonFootprint] = useState(0)
   const [foodCarbonFootprint, setFoodCarbonFootprint] = useState(0)
   const [transportCarbonFootprint, setTransportCarbonFootprint] = useState(0)
+  const [devicesCarbonFootprint, setDeviceCarbonFootprint] = useState(0)
   const [isSubmitted, setIsSubmitted] = useState(false)
 
   const handleKilometersChange = (e) => {
@@ -19,23 +22,41 @@ const Form = () => {
   const handleFlightHoursChange = (e) => {
     setFlightHours(e.target.value)
   }
+  const handleLaptopChange = (e) => {
+    setLaptop(e.target.value)
+  }
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value)
+  }
   const calculateCarbonFootprint = () => {
     // Facteurs d'émission fictifs
     const emissionPerKilometer = 120
     const emissionPerGramOfMeat = 25
     const emissionPerFlightHour = 200
+    const emissionPerLaptop = 200
+    const emissionPerPhone = 50
 
     const transportCarbonFromKilometers = kilometers * emissionPerKilometer
     const foodCarbonFromMeat = meatConsumed * emissionPerGramOfMeat
     const transportCarbonFromFlights = flightHours * emissionPerFlightHour
+    const devicesCarbonFromLaptop = laptop * emissionPerLaptop
+    const devicesCarbonFromPhone = phone * emissionPerPhone
 
-    // Total empreinte carbone transport
+    // Calcul de l'empreinte carbone transport
     const totalTransportCarbonFootprint =
       transportCarbonFromKilometers + transportCarbonFromFlights
     setTransportCarbonFootprint(totalTransportCarbonFootprint)
     setFoodCarbonFootprint(foodCarbonFromMeat)
 
-    const totalCarbon = totalTransportCarbonFootprint + foodCarbonFromMeat
+    // Calcul de l'empreinte carbone des appareils portables
+    const totalDevicesCarbonFootprint =
+      devicesCarbonFromLaptop + devicesCarbonFromPhone
+    setDeviceCarbonFootprint(totalDevicesCarbonFootprint)
+
+    const totalCarbon =
+      totalTransportCarbonFootprint +
+      foodCarbonFromMeat +
+      totalDevicesCarbonFootprint
     setTotalCarbonFootprint(totalCarbon)
   }
 
@@ -45,6 +66,8 @@ const Form = () => {
     setFlightHours('')
     setKilometers('')
     setMeatConsumed('')
+    setLaptop('')
+    setPhone('')
     setIsSubmitted(true)
   }
 
@@ -54,7 +77,7 @@ const Form = () => {
         <label>
           Km parcourus par semaine en voiture:
           <input
-            type="text"
+            type="number"
             value={kilometers}
             onChange={handleKilometersChange}
           />
@@ -63,7 +86,7 @@ const Form = () => {
         <label>
           Quantité de viande / semaine (en grammes):
           <input
-            type="text"
+            type="number"
             value={meatConsumed}
             onChange={handleMeatConsumedChange}
           />
@@ -72,10 +95,20 @@ const Form = () => {
         <label>
           Nombre d'heures de vol par an:
           <input
-            type="text"
+            type="number"
             value={flightHours}
             onChange={handleFlightHoursChange}
           />
+        </label>
+        <br />
+        <label>
+          Nombre d'ordinateurs portables:
+          <input type="number" value={laptop} onChange={handleLaptopChange} />
+        </label>
+        <br />
+        <label>
+          Nombre de téléphones portables:
+          <input type="number" value={phone} onChange={handlePhoneChange} />
         </label>
         <br />
         <button type="submit">Calculer</button>
@@ -97,11 +130,19 @@ const Form = () => {
                 )}
               </p>
             )}
-            {foodCarbonFootprint > 0 && (
+            {transportCarbonFootprint > 0 && (
               <p className={styles.category_result}>
                 Transport :
                 {transportCarbonFootprint > 0 && (
                   <span> {transportCarbonFootprint} g de CO2</span>
+                )}
+              </p>
+            )}
+            {devicesCarbonFootprint > 0 && (
+              <p className={styles.category_result}>
+                Appareils portables:
+                {devicesCarbonFootprint > 0 && (
+                  <span> {devicesCarbonFootprint} g de CO2</span>
                 )}
               </p>
             )}
